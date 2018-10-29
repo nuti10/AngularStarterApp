37d4e8035f1c0311b7d4d3bb7afdf8e316ffab39
Nupur Tejwani <nupurtejwani@gmail.com>
2018-10-28 22:58:59 -0400

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PricingDashboardComponent } from './pricing-dashboard/pricing-dashboard/pricing-dashboard.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PricingDashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
